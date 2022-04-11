535d453d936552dbe6844eef78e3e8be07ee2a6d
Istiaq Nirab <40653908+Ok9xNirab@users.noreply.github.com>
2022-04-11 18:54:03 +0600

    clean.sh added

---
+++ clean.sh
@@ -0,0 +1,12 @@
+composer dump-autoload
+rm -r composer.json
+rm -r composer.lock
+
+rm -r README.md
+
+rm -rf .git/
+rm -r .editorconfig
+rm -r .gitignore
+
+echo "Production Ready 📦"
+rm -r clean.sh
