488d87df4440be51488a05b83d22483210523946
Istiaq Nirab <ok9xnirab@gmail.com>
2024-04-03 11:15:47 +0600

    cleanup.

--- clean.sh
+++ clean.sh
@@ -8,12 +8,6 @@ rm -rf .git/
 rm -r .editorconfig
 rm -r .gitignore
 rm -r phpcs.xml
-rm -r .travis.yml
-
-rm -rf cypress/
-rm -r cypress.config.js
-rm -r package.json
-rm -r yarn.lock
 
 echo "Production Ready 📦"
 rm -r clean.sh
